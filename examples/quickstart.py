1080bb0363117a420497b4b8b7a06161ef4d4490
aybanda <ajaybandaru17@gmail.com>
2025-06-26 21:52:59 +0530

    feat: add GCS and Firestore support to pipeline

--- examples/quickstart.py
+++ examples/quickstart.py
@@ -6,10 +6,19 @@ from serenade_flow import pipeline
 print("\nExecuting Quickstart Example\n")
 
 # Configure ETL Pipeline
+#
+# To use the remote data source, you need to:
+# 1. Set the `data_source` to "remote".
+# 2. Provide the GCS path to your data in `data_source_path`.
+# 3. Ensure you have authenticated with GCP. For example, by setting
+#    the GOOGLE_APPLICATION_CREDENTIALS environment variable.
 pipeline.configure({
-    "data_source": "local",
-    "data_source_path": "/path/to/directory",
-    "data_format": "csv"
+    "data_source": "remote",
+    "data_source_path": "gs://your-bucket-name/df/",
+    "data_format": "json",
+    "load_destination": "firestore",
+    "project_id": "your-gcp-project-id",
+    "collection_name": "sports_events_test"
 })
 
 # Extract
