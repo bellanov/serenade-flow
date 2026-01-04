1fe9de89c89060e7b78b4569c0428487faafc358
Bellanov <bellanovapilli@gmail.com>
2026-01-03 21:52:20 -0500

    Addressed Linting Errors

--- tests/pipeline/test_pipeline.py
+++ tests/pipeline/test_pipeline.py
@@ -2,7 +2,7 @@
 
 # import pytest
 
-from serenade_flow import pipeline
+# from serenade_flow import pipeline
 
 # TODO: Schemas for validation can be migrated to the point of execution
 # The unit test should essentially be a mini quickstart example
